c7310d63c44590b409ba6f06a2aa02b28ec90ef1
Kevin Hubilla <khubilla@standard-insurance.com>
2017-02-22 19:15:12 +0800

    Init commit

---
+++ src/main/java/com/standard/beans/Section.java
@@ -0,0 +1,13 @@
+package com.standard.beans;
+
+public class Section {
+	private Student student;
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+}
